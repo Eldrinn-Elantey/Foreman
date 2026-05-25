5102c0a0146718ad22384c99118fc2a95a2902d2
Eldrinn-Elantey <46845681+Eldrinn-Elantey@users.noreply.github.com>
2026-05-25 08:10:29 +0400

    feat: send team members to client on login (SU optional)

--- src/main/java/com/eldrinn/foreman/event/PlayerLoginHandler.java
+++ src/main/java/com/eldrinn/foreman/event/PlayerLoginHandler.java
@@ -1,11 +1,17 @@
 package com.eldrinn.foreman.event;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import net.minecraft.entity.player.EntityPlayerMP;
 
+import com.eldrinn.foreman.cache.PlayerEntry;
 import com.eldrinn.foreman.network.ForemanNetwork;
 import com.eldrinn.foreman.network.SyncAllTasksPacket;
+import com.eldrinn.foreman.network.SyncTeamMembersPacket;
 import com.eldrinn.foreman.storage.ForemanWorldData;
 
+import cpw.mods.fml.common.Loader;
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
 import cpw.mods.fml.common.gameevent.PlayerEvent;
 
@@ -17,5 +23,11 @@ public class PlayerLoginHandler {
         EntityPlayerMP player = (EntityPlayerMP) event.player;
         ForemanWorldData data = ForemanWorldData.get();
         ForemanNetwork.CHANNEL.sendTo(new SyncAllTasksPacket(data.getAllTasks()), player);
+        ForemanNetwork.CHANNEL.sendTo(new SyncTeamMembersPacket(resolveTeamMembers(player)), player);
+    }
+
+    private List<PlayerEntry> resolveTeamMembers(EntityPlayerMP player) {
+        if (!Loader.isModLoaded("serverutilities")) return new ArrayList<>();
+        return ServerUtilitiesTeamHelper.getTeamMembers(player);
     }
 }
